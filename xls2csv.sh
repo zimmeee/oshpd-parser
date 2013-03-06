846bd5fbfa62c6b0ff8bf0831e0c560c30debc39
zimmeee <nzimmerman@gmail.com>
2013-03-06 12:46:04 -0800

    Improved comments

--- xls2csv.sh
+++ xls2csv.sh
@@ -1,3 +1,7 @@
+# Converts all the file names to uppercase
+# Then converts all the XLS and XLSX files to CSV
+# Usage: ./xls2csv.py <dir>
+
 if test "$1" == "" ; then
 	echo $'\a'You forgot to specify the directory with the xls files to be converted
 	echo $0 dir/with/xls/files
